211734caa8bcd7ee60ec8470460e5c967399360f
Alperen Yanık <alperenyanik@hotmail.com>
2024-04-24 12:59:40 +0300

    feat: create contact application

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myCV.apps.MycvConfig',
+    "myCV.apps.MycvConfig",
+    "contact",
 ]
 
 MIDDLEWARE = [
