fde79a6cf7ec8eff65cb12ffef56660063b4817a
Alperen Yanık <alperenyanik@hotmail.com>
2024-05-01 21:17:08 +0300

    refactor: changes on ready template

--- myCV/models.py
+++ myCV/models.py
@@ -51,3 +51,35 @@ class GeneralSetting(AbstractModel):
         verbose_name = "General Setting"
         verbose_name_plural = "General Settings"
         ordering = ("name",)
+
+
+class ImageSetting(AbstractModel):
+    name = models.CharField(
+        default="",
+        max_length=254,
+        blank=True,
+        verbose_name="Name",
+        help_text="This is variable of the setting.",
+    )
+    description = models.CharField(
+        default="",
+        max_length=254,
+        blank=True,
+        verbose_name="Description",
+        help_text="",
+    )
+    file = models.ImageField(
+        default="",
+        verbose_name="Image",
+        help_text="",
+        blank=True,
+        upload_to="images"
+    )
+
+    def __str__(self):
+        return f"Image Setting: {self.name}"
+
+    class Meta:
+        verbose_name = "Image Setting"
+        verbose_name_plural = "Image Settings"
+        ordering = ("name",)
